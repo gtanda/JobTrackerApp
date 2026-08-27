7c5503ef2e1c0dcc69851924e24beec042637f6f
gtanda <govindstanda@outlook.com>
2026-08-26 21:28:21 -0700

    Created jobentry DTOs create,update,response

---
+++ backend/Models/DTOs/CreateJobEntryDto.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using backend.Models.Enums;
+
+namespace backend.Models.DTOs;
+
+public record CreateJobEntryDto
+{
+    [Required]
+    [MaxLength(100)]
+    public required string CompanyName { get; init; }
+    
+    [Required]
+    [MaxLength(100)]
+    public required string JobTitle { get; init; }
+    public DateTime? DateApplied { get; init; }
+    public ApplicationStatus ApplicationStatus {get; init;}
+    
+
+    public string? Notes { get; init; }
+    public JobSource? JobSource {get; init;}
+    [MaxLength(2000)]
+    public string? PostingUrl { get; init; }
+    [Range(0.0, 10_000_000.0)]
+    public decimal? SalaryMin { get; init; }
+    [Range(0.0, 10_000_000.0)]
+    public decimal? SalaryMax { get; init; }
+    
+    [MaxLength(100)]
+    public string? RecruiterName { get; init; }
+    [MaxLength(254)]
+    [EmailAddress]
+    public string? RecruiterEmail { get; init; }
+    public DateTime? InterviewDate  { get; init; }
+}
---
+++ backend/Models/DTOs/JobEntryResponseDto.cs
@@ -0,0 +1,31 @@
+﻿using backend.Models.Enums;
+
+namespace backend.Models.DTOs;
+
+public record JobEntryResponseDto
+{
+    public Guid JobEntryId { get; init; }
+    
+    public required string CompanyName { get; init; }
+    
+
+    public required string JobTitle { get; init; }
+    public DateTime? DateApplied { get; init; }
+    public ApplicationStatus ApplicationStatus {get; init;}
+    
+
+    public string? Notes { get; init; }
+    public JobSource? JobSource {get; init;}
+
+    public string? PostingUrl { get; init; }
+
+    public decimal? SalaryMin { get; init; }
+
+    public decimal? SalaryMax { get; init; }
+    
+
+    public string? RecruiterName { get; init; }
+
+    public string? RecruiterEmail { get; init; }
+    public DateTime? InterviewDate  { get; init; }
+}
