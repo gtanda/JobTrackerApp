518da51b3fb2684c1e6caac6264747ee7a5cbc47
gtanda <govindstanda@outlook.com>
2026-08-31 22:13:49 -0700

    Added testcontainers and scaffolding out testing

---
+++ backend.Tests/JobEntriesControllerTests.cs
@@ -0,0 +1,23 @@
+﻿using System.Net;
+
+namespace backend.Tests;
+
+public class JobEntriesControllerTests : IClassFixture<JobTrackerWebApplicationFactory>
+{
+    private readonly JobTrackerWebApplicationFactory _factory;
+    private readonly HttpClient _client;
+    
+    public JobEntriesControllerTests(JobTrackerWebApplicationFactory factory)
+    {
+        _client = factory.CreateClient();
+        _factory = factory;
+    }
+
+    [Fact]
+    public async Task GetJobEntries_ReturnsOk()
+    {
+        var response = await _client.GetAsync("/api/JobEntries");
+        
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+    }
+}
---
+++ backend.Tests/JobTrackerWebApplicationFactory.cs
@@ -0,0 +1,33 @@
+﻿using backend.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Testcontainers.PostgreSql;
+
+namespace backend.Tests;
+
+public class JobTrackerWebApplicationFactory : WebApplicationFactory<Program>, IAsyncLifetime
+{
+    private readonly PostgreSqlContainer _postgreSqlContainer = new PostgreSqlBuilder("postgres:18").Build();
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.UseSetting("ConnectionStrings:JobTrackerDB", _postgreSqlContainer.GetConnectionString());
+    }
+    
+    public async Task InitializeAsync()
+    {
+        await _postgreSqlContainer.StartAsync();
+        using var scope = Services.CreateScope();
+        var context = scope.ServiceProvider.GetRequiredService<JobTrackerContext>();
+        await context.Database.MigrateAsync();
+    }
+
+    async Task IAsyncLifetime.DisposeAsync()
+    {
+        await _postgreSqlContainer.StopAsync();
+        await base.DisposeAsync();
+    }
+    
+}
